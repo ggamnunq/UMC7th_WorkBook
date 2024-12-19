035b276d669a804874720226dbf9ba83e018d525
junyong <rlawnsdyd530@gmail.com>
2024-12-20 03:26:21 +0900

    Feat : 10주차 선택미션 - OAuth2를 이용하여 구글 소셜 로그인 구현

--- src/main/java/umc/spring/config/security/CustomOAuth2UserService.java
+++ src/main/java/umc/spring/config/security/CustomOAuth2UserService.java
@@ -13,8 +13,6 @@ import umc.spring.domain.enums.Gender;
 import umc.spring.domain.enums.Role;
 import umc.spring.repository.MemberRepository.MemberRepository;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -29,23 +27,40 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        // 로그인 진행 중인 서비스를 구분
+        // 네이버로 로그인 진행 중인지, 구글로 로그인 진행 중인지, ... 등을 구분
+        // ex) "naver", "google", "kakao"
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-        String nickname = (String) properties.get("nickname");
-        String email = nickname + "@kakao.com";
+        // 필요한 정보를 provider( 구글, 카카오 )에 따라 다르게 mapping
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2User.getAttributes());
 
-        Member member = saveOrUpdateUser(nickname, email);
-
-        // attributes 수정 -> 이메일을 주요 식별자로 설정하기 위해
-        HashMap<String, Object> modifiedAttributes = new HashMap<>(attributes);
-        modifiedAttributes.put("email", email);
+        // 멤버 업데이트 또는 저장
+        saveOrUpdateUser(oAuth2UserInfo.getEmail(), oAuth2UserInfo.getNickname());
 
         return new DefaultOAuth2User(
                 oAuth2User.getAuthorities(),
-                modifiedAttributes,
-                "email" // email을 주요 식별자로 설정
+                oAuth2User.getAttributes(),
+                "email"
         );
+
+//        Map<String, Object> attributes = oAuth2User.getAttributes();
+//        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+//
+//        String nickname = (String) properties.get("nickname");
+//        String email = nickname + "@kakao.com";
+//
+//        Member member = saveOrUpdateUser(nickname, email);
+//
+//        // attributes 수정 -> 이메일을 주요 식별자로 설정하기 위해
+//        HashMap<String, Object> modifiedAttributes = new HashMap<>(attributes);
+//        modifiedAttributes.put("email", email);
+//
+//        return new DefaultOAuth2User(
+//                oAuth2User.getAuthorities(),
+//                modifiedAttributes,
+//                "email" // email을 주요 식별자로 설정
+//        );
     }
 
     private Member saveOrUpdateUser(String email, String nickname) {
---
+++ src/main/java/umc/spring/config/security/OAuth2UserInfo.java
@@ -0,0 +1,57 @@
+package umc.spring.config.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import umc.spring.domain.entity.Member;
+
+import java.util.Map;
+
+@Builder
+@Getter
+@ToString
+public class OAuth2UserInfo {
+
+    private String password;
+    private String email;
+    private String nickname;
+    private String provider;
+
+    public static OAuth2UserInfo of(String provider, Map<String, Object> attributes) {
+        switch (provider) {
+            case "google":
+                return ofGoogle(attributes);
+            case "kakao":
+                return ofKakao(attributes);
+            default:
+                throw new RuntimeException();
+        }
+    }
+
+    private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .password((String) attributes.get("sub"))
+                .nickname((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .build();
+    }
+
+    private static OAuth2UserInfo ofKakao(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .password(attributes.get("id").toString())
+                .nickname((String) ((Map) attributes.get("properties")).get("nickname"))
+                //이메일 정보는 지금 받아오지 않으니까 임시방편으로 해둠
+                .email((String) ((Map) attributes.get("properties")).get("nickname") + "@kakao.com")
+                .build();
+    }
+
+    public Member toEntity(){
+        return Member.builder()
+                .password(password)
+                .email(email)
+                .name(nickname)
+                .build();
+    }
+}
